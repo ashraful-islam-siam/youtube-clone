841a897b283d227160aea8adb7e7cb6b49e49fdf
ashraful-islam-siam <ashrafulislam.dev1@gmail.com>
2023-01-08 22:43:37 +0600

    Initial commit

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
+import { Link, useParams } from 'react-router-dom'
+import ReactPlayer from 'react-player'
+import { Box, Stack, Typography } from '@mui/material'
+
+import { Video } from "./"
+import {fetchFromAPI} from "../utils/fetchDataFromAPI"
+import { CheckCircle } from '@mui/icons-material'
 
 const VideoDetail = () => {
+  const [videoDetail, setVideoDetail] = useState(null)
+  const { id } = useParams()
+
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then(data => setVideoDetail(data.items[0]))
+  }, [id])
+
+  if(!videoDetail?.snippet) return <h1>Loading....</h1>
+  const { snippet: {title, channelId, channelTitle}, statistics: {viewCount, likeCount}} = videoDetail;
   return (
-    <div>VideoDetail</div>
+    <Box minHeight={"95vh"}>
+      <Stack direction={{xs: 'column', md: "row"}}>
+        <Box flex={1}>
+          <Box sx={{width: "100%", position: "sticky", top: "86px"}}>
+            <ReactPlayer src={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls/>
+            <Typography variant="h5" fontWeight="bold" color="#fff" p={2}>
+              {title}
+            </Typography>
+            <Stack direction={"row"} justifyContent="space-between" sx={{
+              color: "#fff"
+            }} py={1} px={2}>
+              <Link to={`/channel/${channelId}`}>
+              <Typography variant={{sm: "subtitle1", md: "h6"}} color="#fff">
+                {channelTitle}
+                <CheckCircle sx={{fontSize: "12px", color: "gray", ml: "5px"}} />
+              </Typography>
+              </Link>
+              <Stack direction={"row"} gap="20px" alignItems={"center"}>
+                <Typography variant="body1" sx={{opacity: 0.7}}>
+                  {parseInt(viewCount).toLocaleString()} viwes
+                </Typography>
+                <Typography variant="body1" sx={{opacity: 0.7}}>
+                  {parseInt(likeCount).toLocaleString()} likes
+                </Typography>
+              </Stack>
+            </Stack>
+          </Box>
+        </Box>
+      </Stack>      
+    </Box>
   )
 }
 
--- src/utils/fetchDataFromAPI.jsx
+++ src/utils/fetchDataFromAPI.jsx
@@ -1,15 +1,17 @@
 import axios from "axios"
-const BASE_URL = "https://youtube-v31.p.rapidapi.com/captions"
+const BASE_URL = "https://youtube-v31.p.rapidapi.com"
 const options = {
     method: 'GET',
     url: BASE_URL,
     params: {maxResults: 50},
     headers: {
-      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+      'X-RapidAPI-Key': "605c63dbd6mshb26914674222b0ep1eef3cjsn2cc536c067c9",
       'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
     }
   };
 
 export const fetchFromAPI = async (url) => {
-    await axios.get(`${BASE_URL}`)
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options)
+
+    return data
 }
